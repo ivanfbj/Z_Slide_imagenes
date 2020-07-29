3386a3a56ff640bc3b0f9911fe038febdf58020f
Ivan Fernando Bedoya Jaramillo <ivanfbj@gmail.com>
2020-07-28 20:31:12 -0500

    V1_base del desarrollo

---
+++ index.js
@@ -0,0 +1,27 @@
+var images = [
+  "./images/mago_oscuro.png",
+  "./images/craneo.png",
+  "./images/dragon_azul.png",
+  "./images/exodia.png",
+  "./images/kuriboh.png",
+  "./images/maga_oscuro.png"
+];
+var num = 0;
+
+function next() {
+  var slider = document.getElementById("slider");
+  num++;
+  if (num >= images.length) {
+    num = 0;
+  }
+  slider.src = images[num];
+}
+
+function prev() {
+  var slider = document.getElementById("slider");
+  num--;
+  if (num < 0) {
+    num = images.length - 1;
+  }
+  slider.src = images[num];
+}
